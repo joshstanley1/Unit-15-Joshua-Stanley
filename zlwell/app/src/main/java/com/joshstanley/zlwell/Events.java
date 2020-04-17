125f9324dde8d0200d3788c74be464ad9b389cf8
joshstanley1 <579158@students.gateshead.ac.uk>
2020-04-17 20:02:40 +0100

    updated oncreateview for events class

--- zlwell/app/src/main/java/com/joshstanley/zlwell/Events.java
+++ zlwell/app/src/main/java/com/joshstanley/zlwell/Events.java
@@ -44,7 +44,39 @@ public class Events extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+        eventScrollView = view.findViewById(R.layout.sv_events);
+        LinearLayout eventsContainer = eventScrollView.findViewById.event_sv_li);
+        try{
+            events = generateEvents();
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        for(Event e : events){
+            calendarEventView = inflater.inflate(R.layout.calendar_event, container, false);
+            eventDate = calendarEventView.findViewById(R.id.tv_calendar_event_date);
+            eventDate.setText(e.date);
+            eventItemLayout = calendarEventView.findViewById(R.id.ll_calendar_event_items);
+            eventItem = (LinearLayout)inflater.inflate(R.layout.event_item, container, false);
+            eventTime = eventItem.findViewById(R.id.event_time);
+            eventSubject = eventItem.findViewById(R.id.event_subject);
+            eventLocation = eventItem.findViewById(R.id.event_location);
+            eventTime.setText(e.time);
+            eventSubject.setText(e.subject);
+            eventLocation.setText(e.location);
+            eventItemLayout.addView(eventItem);
+            eventsContainer.addView(eventItemLayout);
+
+        }
+        return view;
+    }
+
+    private String formatDate(String date) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        Date newDate = sdf.parse(date);
+        sdf = new SimpleDateFormat("dd/MMM", Locale.ENGLISH);
+        return sdf.format(newDate);
     }
 
     private ArrayList<Event> generateEvents() throws ParseException{
@@ -56,10 +88,5 @@ public class Events extends Fragment {
         return events;
     }
 
-    private String formatDate(String date) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date newDate = sdf.parse(date);
-        sdf = new SimpleDateFormat("dd/MMM", Locale.ENGLISH);
-        return sdf.format(newDate);
-    }
+
 }
